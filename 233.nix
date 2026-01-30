f6a0434ee63434bf28ce3b5c6dd2b6f6370505a2
Flandery <fflandery@outlook.com>
2026-01-30 22:37:43 +0800

    change zram config

--- 233.nix
+++ 233.nix
@@ -102,6 +102,13 @@
 
 #zram
  zramSwap.enable = true; # Creates a zram block device and uses it as a swap device
+ boot.kernelParams = [
+    "zswap.enabled=1" # enables zswap
+    "zswap.compressor=lz4" # compression algorithm
+    "zswap.max_pool_percent=100" # maximum percentage of RAM that zswap is allowed to use
+    "zswap.shrinker_enabled=1" # whether to shrink the pool proactively on high memory pressure
+ ];
+
 
 # 启用自动垃圾收集
 # ......
