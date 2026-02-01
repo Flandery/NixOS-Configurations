1c9bb27a4b11923b09b1b264d8573f4b33c35bcf
Flandery <fflandery@outlook.com>
2026-02-01 11:27:55 +0800

    use zen kernel instead

--- configuration.nix
+++ configuration.nix
@@ -14,9 +14,8 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
   # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
 
   # networking.hostName = "nixos"; # Define your hostname.
 
