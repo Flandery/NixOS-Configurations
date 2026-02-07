1f7b0bebdb4b775bb934ed7dd93c8ed66fbc4805
Flandery <fflandery@outlook.com>
2026-02-07 17:54:12 +0800

    add bash to 233.nix

--- 233.nix
+++ 233.nix
@@ -12,6 +12,7 @@
  environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+    bash
     neovim
     prismlauncher
     vscode
