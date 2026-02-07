05e8b3b6a39865f36f4695a795c857f34e05b32f
Flandery <fflandery@outlook.com>
2026-02-07 12:01:23 +0800

    change to lts kernel

--- configuration.nix
+++ configuration.nix
@@ -15,7 +15,9 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_zen;
+  #boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.kernelPackages = pkgs.linuxPackages;
+  #boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # networking.hostName = "nixos"; # Define your hostname.
 
